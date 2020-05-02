b2d21bce27f9c1d218feccbb8e275abff7d028c0
tofu18 <fluxatur@gmail.com>
2020-05-02 15:32:38 +0300

    1.0

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя или', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
@@ -47,7 +47,7 @@ class MessageForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Имя пользователя')
-    about = TextAreaField('О себе')
+    about = StringField('О себе')
     location = StringField('Ваше местоположение')
     image = FileField()
     submit = SubmitField('Изменить')
@@ -104,6 +104,7 @@ def login():
 @app.route('/create_question', methods=['GET', 'POST'])
 @login_required
 def create_question():
+    """Страница с формой для создания нового вопроса"""
     form = QuestionForm()
     if form.validate_on_submit():
         question = Question(creator_id=current_user.id, text=form.text.data)
@@ -124,12 +125,13 @@ def create_question():
 @login_required
 def logout():
     logout_user()
-    return redirect("/")
+    return redirect("/login")
 
 
 @app.route('/')
 @login_required
 def index():
+    """Основная стрпница, на которой отображаются все вопросы """
     session = create_session()
     questions = session.query(Question).all()
     return render_template('index.html', questions=questions, user=current_user)
@@ -138,6 +140,7 @@ def index():
 @app.route('/profile/<int:user_id>')
 @login_required
 def profile(user_id):
+    """Страница с данными об указанном профиле"""
     session = create_session()
     user = session.query(User).filter(User.id == user_id).first()
     return render_template('profile.html', user=user)
@@ -146,6 +149,7 @@ def profile(user_id):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+    """Изменение информации текущего пользователя"""
     form = EditProfileForm()
     if form.validate_on_submit():
         session = create_session()
@@ -155,17 +159,18 @@ def edit_profile():
         current_user.location = location
         image = form.image.data
         f = image.read()
-        if image != current_user.profile_image and f != b'':
+        if image != current_user.profile_image and f != b'':  # Загружает новую картинку и удаляет страрую
             filename = f'static/img/{random.randrange(1, 2 ** 20)}.jpg'
             with open(filename, mode='wb') as file:
                 file.write(f)
             os.remove(current_user.profile_image)
             current_user.profile_image = filename
-        if location != None:
+        if location != None:  #
             response = get(
                 f'https://geocode-maps.yandex.ru/1.x?geocode={location}&apikey=40d1649f-0493-4b70-98ba-98533de7710b&format=json')
-            if response:
-                response = json.loads(response.content)
+            response = json.loads(response.content)
+            if response['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData'][
+                'found'] != '0':  # Проверяет, существует ли указанная локация, если да, то загружает её карту
                 coords = response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
                     'Point']['pos'].split()
                 bbox = \
@@ -182,7 +187,9 @@ def edit_profile():
                     else:
                         with open(current_user.location_image, mode='wb') as f:
                             f.write(location_map.content)
-        session.merge(current_user)
+            else:
+                current_user.location_image = None
+            session.merge(current_user)
         session.commit()
         return redirect(f'/profile/{current_user.id}')
 
@@ -192,12 +199,14 @@ def edit_profile():
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 @login_required
 def question(question_id):
+    """Страница с отображением вопроса и ответов на него"""
     form = MessageForm()
     session = create_session()
     question = session.query(Question).filter(Question.id == question_id).first()
     if form.validate_on_submit():
         message = session.query(Message).filter(Message.id == form.reply_to.data).first()
-        if (message and message in question.messages) or form.reply_to.data == '':
+        if (
+                message and message in question.messages) or form.reply_to.data == '':  # Проверяет, находится ли сообщение, на которое отвечает данное сообщение, в данном вопросе
             message = Message(creator_id=current_user.id, reply_to_question=question_id, text=form.message_text.data,
                               reply_to=form.reply_to.data)
             session.add(message)
@@ -210,6 +219,7 @@ def question(question_id):
 @app.route('/give_mod_rights/<id>')
 @login_required
 def give_mod_rights(id):
+    """Даёт указанному пользователю права модератора (работает только если id текущего пользователя равен 1)"""
     if current_user.id == 1:
         session = create_session()
         user = session.query(User).filter(User.id == id).first()
@@ -219,14 +229,28 @@ def give_mod_rights(id):
     return redirect(f'/profile/{id}')
 
 
+@app.route('/remove_mod_rights/<id>')
+@login_required
+def remove_mod_rights(id):
+    """Убирает права модератора у указанного пользователя (работает только если id текущего пользователя равен 1)"""
+    if current_user.id == 1:
+        session = create_session()
+        user = session.query(User).filter(User.id == id).first()
+        user.admin = False
+        session.merge(user)
+        session.commit()
+    return redirect(f'/profile/{id}')
+
+
 @app.route('/delete_question/<id>')
 @login_required
 def delete_question(id):
+    """Удаляет заданный вопрос (работает только с правами модератора)"""
     if current_user.admin == 1:
         session = create_session()
         question = session.query(Question).filter(Question.id == id).first()
         if question.pinned_image != None:
-            os.remove(question.pinned_image)
+            os.remove(question.pinned_image)  # Удаление приложенной картинки с сервера
         session.delete(question)
         session.commit()
     return redirect('/')
@@ -235,6 +259,7 @@ def delete_question(id):
 @app.route('/delete_message/<id>')
 @login_required
 def delete_message(id):
+    """Удаляет заданное сообщение (работает только с правами модератора)"""
     if current_user.admin == 1:
         session = create_session()
         message = session.query(Message).filter(Message.id == id).first()
@@ -247,6 +272,7 @@ def delete_message(id):
 @app.route('/delete_user/<int:id>')
 @login_required
 def delete_user(id):
+    """Удаляет заданного пользователя (работает только с правами модератора)"""
     if current_user.admin == 1 or current_user.id == id:
         session = create_session()
         user = session.query(User).filter(User.id == id).first()
@@ -256,7 +282,7 @@ def delete_user(id):
             session.delete(user)
             session.commit()
 
-    if current_user.id == id:
+    if current_user.id == id:  # Если пользователь удаляет сам себя, происходит редирект на страницу регистрации
         return redirect('/register')
     return redirect('/')
 
